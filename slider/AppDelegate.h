91ea418e00989bde471370955a416f9fd658ab94
Zixuan Huang <stznh01@moravian.edu>
2014-01-27 14:35:36 -0500

    Initial Commit

---
+++ slider/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  slider
+//
+//  Created by Hansen on 1/27/14.
+//  Copyright (c) 2014 Zixuan Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ slider/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  slider
+//
+//  Created by Hansen on 1/27/14.
+//  Copyright (c) 2014 Zixuan Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
